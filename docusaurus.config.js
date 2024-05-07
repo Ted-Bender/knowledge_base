7fa7e626ce35156e86643f7bc81836169a73ef65
Ted-Bender <122031815+Ted-Bender@users.noreply.github.com>
2024-05-07 09:50:00 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
             copyright: `
 				<div class="footer__copyright row" style="justify-content: center">
 					<div class="cell" style="padding: 1rem;" >
-						<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a>
+						<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Lizense" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a>
 					</div>
 					<div class="cell" style="padding: 0.1rem; text-align: left;" >
 						Licensed under a <a class="footer__link-item" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons BY-SA 4.0</a> License, if not stated otherwise.<br />
